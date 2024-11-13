dfe486e18a633e78ff4685ec0643c864a8caec05
nia <1205367274@qq.com>
2024-11-13 15:07:13 +0800

    2022 day3 part2

---
+++ topic2022/rucksack_reorganization/rucksack_reorganization_part2.go
@@ -0,0 +1,40 @@
+package rucksack_reorganization
+
+import "fmt"
+
+func sumPrioritiesP2() {
+	input := readInput()
+	if input == nil {
+		return
+	}
+	generatePriorityMap()
+	sum := 0
+	for i := 0; i < len(input); i += 3 {
+		priority := calcPriorityP2(input[i], input[i+1], input[i+2])
+		sum += priority
+	}
+	fmt.Println(sum)
+}
+
+func calcPriorityP2(first, second, third string) int {
+	charMap := make(map[rune]int)
+	for _, char := range first {
+		charMap[char] = 1
+	}
+	for _, char := range second {
+		if charMap[char] == 1 {
+			charMap[char] = 2
+		}
+	}
+	for _, char := range third {
+		if charMap[char] == 2 {
+			charMap[char] = 3
+		}
+	}
+	for key, value := range charMap {
+		if value == 3 {
+			return priorityMap[key]
+		}
+	}
+	return 0
+}
---
+++ topic2022/rucksack_reorganization/rucksack_reorganization_part2_test.go
@@ -0,0 +1,7 @@
+package rucksack_reorganization
+
+import "testing"
+
+func Test_sumPrioritiesP2(t *testing.T) { //2620
+	sumPrioritiesP2()
+}
